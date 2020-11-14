8c45dd909aeb12c0c45e80159f704a18dd7c3fff
Jose Pereira <jose.pereira@mercedes-benz.io>
2020-11-14 22:46:01 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>soon</h1>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
